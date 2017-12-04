40b9110ccdf3611ed32757f0ae5db45eb5026094
Vincent Kwon <hobum.kwon@gmail.com>
2017-12-04 16:40:16 +0900

    Adding connection pool random test

--- Project1/ConnectionManagement.cpp
+++ Project1/ConnectionManagement.cpp
@@ -1,50 +1,125 @@
 #include <iostream>
 #include "RandomDataGenerator.h"
-
+#include <time.h>
 using namespace std;
 
 static char gId[STRING_MAX_ROW][STRING_MAX_LEN];
 static char gPasswd[STRING_MAX_ROW][STRING_MAX_LEN];
-static int  gAction[INT_MAX_SIZE];
-static int  gCar[INT_MAX_SIZE];
+static int gDefaultTime[INT_MAX_SIZE];
 
 #define ID_MIN_LEN 5
 #define ID_MAX_LEN 10
-#define ID_NUM 20
 
-#define PASSWD_LEN 20
-#define PASSWD_NUM 3
+#define PASSWD_MIN_LEN 5
+#define PASSWD_MAX_LEN 10
 
-#define ACTION_MIN 1000
-#define ACTION_MAX 6000
-#define ACTION_NUM 5
+#define DEFAULT_MIN_TIME 1
+#define DEFAULT_MAX_TIME 30000
 
-#define CAR_MIN 2010
-#define CAR_MAX 2018
-#define CAR_NUM 3
+#define NEW_ACCOUNT_CALL_MAX 50000
 
-int connectionManagementTest() {
-	randomVarLenStringGenerator(ID_MIN_LEN, ID_MAX_LEN, ID_NUM, gId, true);
+#define TICK_CALL_MAX 50000
 
-	for (int i = 0; i < ID_NUM; i++)
-		cout << gId[i] << endl;
+enum Actions {
+	NEW_ACCOUNT,
+	CONNECT,
+	LOG_OUT,
+	TICK
+};
 
-	cout << "Next string generation...." << endl;
+void doNewAccount(char* id, char* passwd, int default) {
+	cout << "[NEW] " << id << ":" << passwd << ":" << default << endl;
+}
 
-	randomFixedLenStringGenerator(PASSWD_LEN, PASSWD_NUM, gPasswd, true);
+void doConnect(char* id, char* passwd) {
+	cout << "[Connect] " << id << ":" << passwd << endl;
+}
 
-	for (int i = 0; i < PASSWD_NUM; i++)
-		cout << gPasswd[i] << endl;
+void doLogout(char* id) {
+	cout << "[Logout] " << id << endl;
+}
 
-	randomIntGenerator(ACTION_MIN, ACTION_MAX, ACTION_NUM, gAction, true);
-
-	for (int i = 0; i < ACTION_NUM; i++)
-		cout << gAction[i] << endl;
-
-	randomIntGenerator(CAR_MIN, CAR_MAX, CAR_NUM, gCar, true);
-
-	for (int i = 0; i < CAR_NUM; i++)
-		cout << gCar[i] << endl;
-
+int doTick() {
+	cout << "[Tick] " << endl;
 	return 0;
 }
+
+int connectionManagementTest() {
+	int idOffset = -1;
+	int passwdOffset = -1;
+	int defaultTimeOffset = -1;
+
+	randomInit();
+	for (int i = 0; i < 3; i++) {
+		// generate ID 10000¹ø
+		idOffset = randomVarLenStringGenerator(ID_MIN_LEN, ID_MAX_LEN, 10000, gId, true, idOffset+1);
+		passwdOffset = randomVarLenStringGenerator(PASSWD_MIN_LEN, PASSWD_MAX_LEN, 10000, gPasswd, false, passwdOffset+1);
+		defaultTimeOffset = randomIntGenerator(DEFAULT_MIN_TIME, DEFAULT_MAX_TIME, 10000, gDefaultTime, false, defaultTimeOffset + 1);
+
+		// TODO: fill your account by yourself for 10000 account
+		for (int k = 0; k <= idOffset; k++) {
+			doNewAccount(gId[k], gPasswd[k], gDefaultTime[k]);
+		}
+
+		for (int j = 0; j < 10000; j++) {
+			int tmp = rand() % 4;
+
+			if (tmp == NEW_ACCOUNT) {
+				idOffset = randomVarLenStringGenerator(ID_MIN_LEN, ID_MAX_LEN, 1, gId, true, idOffset + 1);
+				passwdOffset = randomVarLenStringGenerator(PASSWD_MIN_LEN, PASSWD_MAX_LEN, 1, gPasswd, false, passwdOffset + 1);
+				defaultTimeOffset = randomIntGenerator(DEFAULT_MIN_TIME, DEFAULT_MAX_TIME, 1, gDefaultTime, false, defaultTimeOffset + 1);
+
+				// TODO: fill your account by yourself for one account
+				doNewAccount(gId[idOffset], gPasswd[passwdOffset], gDefaultTime[defaultTimeOffset]);
+			}
+			else if (tmp == CONNECT) {
+				// select ID and passwd (sometimes wrong)
+				int index = randomIntSelector(0, idOffset);
+
+				// TODO: handle connect request
+				if (time(NULL) % 2)
+					doConnect(gId[index], gPasswd[index]);
+				else
+					doConnect(gId[index], gId[index]);
+			}
+			else if (tmp == LOG_OUT) {
+				//log out this user. if no user exists for the id, ignore
+				// select ID and logout
+				int index = randomIntSelector(0, idOffset);
+
+				// TODO: handle logout
+				doLogout(gId[index]);
+			}
+			else { // tmp is Tick
+				//reduce all remaining time by 1
+				doTick();
+			}
+		}
+		// random(all) + tick 10000¹ø
+	}
+	return 0;
+}
+//	randomVarLenStringGenerator(ID_MIN_LEN, ID_MAX_LEN, ID_NUM, gId, true);
+//
+//	for (int i = 0; i < ID_NUM; i++)
+//		cout << gId[i] << endl;
+//
+//	cout << "Next string generation...." << endl;
+//
+//	randomFixedLenStringGenerator(PASSWD_LEN, PASSWD_NUM, gPasswd, true);
+//
+//	for (int i = 0; i < PASSWD_NUM; i++)
+//		cout << gPasswd[i] << endl;
+//
+//	randomIntGenerator(ACTION_MIN, ACTION_MAX, ACTION_NUM, gAction, true);
+//
+//	for (int i = 0; i < ACTION_NUM; i++)
+//		cout << gAction[i] << endl;
+//
+//	randomIntGenerator(CAR_MIN, CAR_MAX, CAR_NUM, gCar, true);
+//
+//	for (int i = 0; i < CAR_NUM; i++)
+//		cout << gCar[i] << endl;
+//
+//	return 0;
+//}
--- Project1/RandomDataGenerator.h
+++ Project1/RandomDataGenerator.h
@@ -5,8 +5,15 @@
 
 #define INT_MAX_SIZE 100000
 
+// call only once per app
+void randomInit();
+
+int randomIntSelector(int minIndex, int maxIndex);
+
 // length means actual string length based on strlen so +1 is needded to buffer
-int randomFixedLenStringGenerator(int length, int numOf, char strArray[STRING_MAX_ROW][STRING_MAX_LEN], bool unique = false);
+int randomFixedLenStringGenerator(int length, int numOf, char strArray[STRING_MAX_ROW][STRING_MAX_LEN], bool unique = false, int offset = 0);
+
 // length means actual string length based on strlen so +1 is needded to buffer
-int randomVarLenStringGenerator(int minLength, int maxLength, int numOf, char strArray[STRING_MAX_ROW][STRING_MAX_LEN], bool unique = false);
-int randomIntGenerator(int min, int max, int numOf, int* intArray, bool unique = false);
+int randomVarLenStringGenerator(int minLength, int maxLength, int numOf, char strArray[STRING_MAX_ROW][STRING_MAX_LEN], bool unique = false, int offset = 0);
+
+int randomIntGenerator(int min, int max, int numOf, int* intArray, bool unique = false, int offset = 0);
